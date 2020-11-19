e244a564d3a98daeff2e05ee2195b0b7dce2eb96
BorisJsdi <boris.jenicek@sdinformatika.hr>
2020-11-19 22:03:13 +0100

    Improvement

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 class Carousel {
 
     ElementID;
+    DefaultIndex = 0;
+    Infinity     = true;
+    List         = [];
+
+    CarouselWidth = 0;
     DOMCarouselWrapper;
     DOMCarouselInnerWrapper;
     DOMCarousel;
     DOMBulletWrapper;
-    DefaultIndex = 0;
-    List = [];
-
-    addBlock(Block) {
-        this.List.push(Block);
-    }
+    DOMPrevButton;
+    DOMNextButton;
 
     ignite() {
         this.DOMCarouselWrapper      = document.querySelector('#' + this.ElementID);
@@ -20,50 +21,62 @@ class Carousel {
         this.DOMCarouselWrapper.innerHTML += `<ul class="bullets-wrapper"></ul>`;
         this.DOMCarousel             = this.DOMCarouselWrapper.children[1].firstChild;
         this.DOMCarouselInnerWrapper = this.DOMCarouselWrapper.children[1];
+        this.DOMPrevButton           = this.DOMCarouselWrapper.children[0];
+        this.DOMNextButton           = this.DOMCarouselWrapper.children[2];
         this.DOMBulletWrapper        = this.DOMCarouselWrapper.childNodes[this.DOMCarouselWrapper.childNodes.length - 1];
-
 
         this.List.forEach((item, index) => {
             this.DOMCarousel.innerHTML += item.Markup;
             this.DOMBulletWrapper.innerHTML += this.createBullets(item, index);
+            const element = this.DOMCarouselWrapper.children[1].firstChild.children[index];
+            this.CarouselWidth += this.getElementFullWidth(element);
+            console.log(' this.CarouselWidth', this.CarouselWidth);
         });
+
+        console.log(' this.CarouselWidth', this.CarouselWidth);
+
         this.DOMBulletWrapper.addEventListener('click', this.handleBulletClicks());
 
         this.DOMCarouselWrapper.addEventListener('click', this.handleNavClicks());
 
-        this.calculateSetTranslate(this.DefaultIndex);
+        this.DOMCarouselWrapper.addEventListener('keydown', this.handleKeys());
+
+
+        this.carouselMove(this.DefaultIndex);
+    }
+
+    addBlock(Block) {
+        this.List.push(Block);
     }
 
     createBullets(item, index) {
-        return `<li data-block-index="${index}"><span class="dot"></span></li>`;
+        return `<li class="li-dot" data-block-index="${index}"><span class="dot"></span></li>`;
     }
 
-    handleNavClicks = () => (event) => {
-        if (event.target.classList.contains('navigation-prev')) {
-
-            if(this.DefaultIndex > 0){
-                this.DefaultIndex -= 1;
-                this.calculateSetTranslate(this.DefaultIndex);
-            }
-
-        } else if(event.target.classList.contains('navigation-next')){
-
-            if(this.DefaultIndex +1 < this.List.length){
-                this.DefaultIndex += 1;
-                this.calculateSetTranslate(this.DefaultIndex);
-            }
+    scrollNext() {
+        if (this.DefaultIndex + 1 < this.List.length) {
+            this.DefaultIndex += 1;
+            this.carouselMove(this.DefaultIndex);
+        } else if (this.Infinity) {
+            this.DefaultIndex = 0;
+            this.carouselMove(this.DefaultIndex);
         }
-    };
+    }
 
-    handleBulletClicks = () => (event) => {
-        if (event.target.nodeName === 'LI') {
-            this.DefaultIndex = +event.target.dataset.blockIndex;
-            this.calculateSetTranslate(this.DefaultIndex);
+    scrollPrev() {
+        if (this.DefaultIndex > 0) {
+            this.DefaultIndex -= 1;
+            this.carouselMove(this.DefaultIndex);
+        } else if (this.Infinity) {
+            this.DefaultIndex = this.List.length - 1;
+            this.carouselMove(this.DefaultIndex);
         }
-    };
 
-    calculateSetTranslate(index){
-        let translate = 0;
+    }
+
+    carouselMove(index) {
+
+        let translate    = 0;
         const element    = this.DOMCarouselWrapper.children[1].firstChild.children[index];
         const blockWidth = this.getElementFullWidth(element);
 
@@ -82,11 +95,44 @@ class Carousel {
             translate = this.DOMCarouselInnerWrapper.offsetWidth / 2 - (elementsBefore + blockWidth / 2);
 
         }
+        this.dotHighlight();
+        this.navigationVisibility();
         this.setCarouselStyle(translate)
     }
 
     setCarouselStyle(translate) {
         this.DOMCarousel.style.transform = `translateX(${translate}px)`;
+        this.DOMCarousel.style.width     = `${this.CarouselWidth}px`;
+    }
+
+    dotHighlight() {
+        const elementNodeList = document.querySelectorAll('#' + this.ElementID + ' .li-dot');
+        const elementList     = Array.from(elementNodeList);
+
+        elementList.forEach(item => {
+            if (+item.dataset.blockIndex === this.DefaultIndex) {
+                item.classList.add('active');
+            } else {
+                item.classList.remove('active');
+            }
+        });
+
+    }
+
+    navigationVisibility() {
+        if (!this.Infinity) {
+            if (this.DefaultIndex === this.List.length - 1) {
+                this.DOMNextButton.classList.add('hide-me');
+            } else {
+                this.DOMNextButton.classList.remove('hide-me');
+            }
+
+            if (this.DefaultIndex === 0) {
+                this.DOMPrevButton.classList.add('hide-me');
+            } else {
+                this.DOMPrevButton.classList.remove('hide-me');
+            }
+        }
     }
 
     getElementFullWidth(element) {
@@ -98,12 +144,33 @@ class Carousel {
         return elementWidth + elementMargin - elementPadding + elementBorder;
     }
 
-    navigationVisibility(){
-        if(this.DefaultIndex === 0){
-        // todo
+    /*** event handler functions ***/
+    handleNavClicks = () => (event) => {
+        if (event.target.classList.contains('navigation-prev')) {
+            this.scrollPrev();
+        } else if (event.target.classList.contains('navigation-next')) {
+            this.scrollNext();
         }
-    }
+    };
 
+    handleBulletClicks = () => (event) => {
+        if (event.target.nodeName === 'LI') {
+            this.DefaultIndex = +event.target.dataset.blockIndex;
+            this.carouselMove(this.DefaultIndex);
+        }
+    };
+
+    handleKeys = () => (event) => {
+        console.log('event', event);
+        switch (event.key) {
+            case "ArrowLeft":
+                this.scrollPrev();
+                break;
+            case "ArrowRight":
+                this.scrollNext();
+                break;
+        }
+    };
 }
 
 class Block {
@@ -116,16 +183,30 @@ class Block {
     }
 }
 
+/* Carousel 1 */
 const carousel     = new Carousel();
 carousel.ElementID = 'carousel1';
-carousel.addBlock(new Block(`<div><h3>Naslov</h3><p>paragraf</p> <p>HTML</p> <p>HTML</p></div>`));
-carousel.addBlock(new Block(`<img src="assets/images/cat.png"></div>`));
+carousel.addBlock(new Block(`<div style="width: 400px; text-align:center"><h3>HTML block</h3><p>First Paragraph</p> <p>Second Paragraph, Lorem Ipsum is simply dummy text.</p> </div>`));
+carousel.addBlock(new Block(`<img src="assets/images/cat.png" alt="cat picture"></div>`));
 carousel.addBlock(new Block(`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`));
 carousel.addBlock(new Block(`Non formatted text block`));
 carousel.addBlock(new Block(`Non formatted text block`));
 carousel.addBlock(new Block(`Non formatted text block`));
 carousel.addBlock(new Block(`Non formatted text block`));
-
-
-
 carousel.ignite();
+
+
+/* Carousel 2 */
+const carousel2        = new Carousel();
+carousel2.ElementID    = 'carousel2';
+carousel2.DefaultIndex = 2;
+carousel2.Infinity     = false;
+
+carousel2.addBlock(new Block(`<div style="width: 400px; text-align:center"><h3>HTML block</h3><p>First Paragraph</p> <p>Second Paragraph, Lorem Ipsum is simply dummy text.</p> </div>`));
+carousel2.addBlock(new Block(`<img src="assets/images/cat.png" alt="cat picture"></div>`));
+carousel2.addBlock(new Block(`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`));
+carousel2.addBlock(new Block(`Non formatted text block`));
+carousel2.addBlock(new Block(`Non formatted text block`));
+carousel2.addBlock(new Block(`Non formatted text block`));
+carousel2.addBlock(new Block(`Non formatted text block`));
+carousel2.ignite();
